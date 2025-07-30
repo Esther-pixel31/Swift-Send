a5fcd0d722c072259444a27ed2ff33b66ee425be
EstherMutua <esthermutua16@gmail.com>
2025-07-30 22:17:31 +0300

    cascading delete on otp

--- Backend/app/models/otp_code.py
+++ Backend/app/models/otp_code.py
@@ -1,13 +1,18 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from datetime import datetime, timedelta
 from .base import Base, SerializationMixin
+from sqlalchemy.orm import relationship
+
 
 class OTPCode(Base, SerializationMixin):
     __tablename__ = 'otp_codes'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     code = Column(String, nullable=False)
     expires_at = Column(DateTime, nullable=False)
     verified = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="otp_codes")  # ✅ Add this
+
